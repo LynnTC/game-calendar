1f3930ff3a758c41c3d62b5645a611b86a9581a4
LynnT <LynnTCai@gmail.com>
2024-02-29 18:26:30 -0600

    css with borrowed...animation test, needs adjustment

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,19 +9,23 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      &nbsp; | &nbsp;
-      <Link to="/calendar/user">Personal Calendar</Link>
-      &nbsp;&nbsp;
-      {user ? (
-        <>
-          <span>Welcome, {user.name}</span>
-          &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
-        </>
-      ) : (
-        <Link to="/login">Log In or Sign Up</Link>
-      )}
+    <nav className='cyber-glitch-0'>
+      <div className="left-links">
+        <Link to="/">Home</Link>
+        &nbsp;&nbsp;
+        <Link to="/calendar/user">Personal Calendar</Link>
+      </div>
+      <div className="right-links">
+        {user ? (
+          <>
+            <span>Welcome, {user.name}</span>
+            &nbsp;&nbsp;<Link to="" onClick={handleLogOut} className="logout">Log Out</Link>
+          </>
+        ) : (
+          <Link to="/login">Log In or Sign Up</Link>
+        )}
+      </div>
     </nav>
   );
 }
+
